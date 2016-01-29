51ae79ffd69b9d0987c85350d72e23c8f3942bfa
sanemat <o.gata.ken@gmail.com>
2016-01-29 18:47:11 +0900

    chore(release): update release script

--- bin/release-circleci.js
+++ bin/release-circleci.js
@@ -13,13 +13,16 @@ const params = Object.assign(
 );
 const yargs = require('yargs');
 const path = require('path');
+const assert = require('assert');
 const ghrOptions = {
   tag: process.env.CIRCLE_TAG,
   ghrPath: path.join(process.cwd(), 'bin', 'ghr-linux_amd64'),
+  targetPath: path.join(process.cwd(), 'dist', 'pkg'),
 };
 const argv = yargs.default(ghrOptions).argv;
+assert(argv.tag !== undefined && argv.tag !== '');
 
-const ghr = spawn(argv.ghrPath, [argv.tag, 'install'], params);
+const ghr = spawn(argv.ghrPath, [argv.tag, argv.targetPath], params);
 ghr.on('error', (err) => {
   console.error(err);
   process.exit(1);
