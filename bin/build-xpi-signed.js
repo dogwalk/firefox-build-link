d5708db0477fd77a86b90817555f3c34f529bae2
sanemat <o.gata.ken@gmail.com>
2016-01-30 10:26:47 +0900

    chore(build): signed build

---
+++ bin/build-xpi-signed.js
@@ -0,0 +1,60 @@
+#!/usr/bin/env node
+
+/* eslint-disable no-console */
+
+// Use AMO_API_KEY and AMO_API_SECRET
+
+const spawn = require('cross-spawn-async');
+const mkdirp = require('mkdirp');
+const cpy = require('cpy');
+const pify = require('pify');
+const path = require('path');
+const yargs = require('yargs');
+const assert = require('assert');
+const params = Object.assign(
+  {},
+  {
+    cwd: 'dist',
+    stdio: 'inherit',
+    env: process.env,
+  }
+);
+const jpmPath = path.join(process.cwd(), 'node_modules', '.bin', 'jpm');
+const signOptions = {
+  apiKey: process.env.AMO_API_KEY,
+  apiSecret: process.env.AMO_API_SECRET,
+};
+const argv = yargs.default(signOptions).argv;
+assert(argv.apiKey !== undefined && argv.apiKey !== '');
+assert(argv.apiSecret !== undefined && argv.apiSecret !== '');
+
+new Promise((resolve, reject) => {
+  const command = spawn(
+    jpmPath,
+    [
+      'sign',
+      '--api-key',
+      argv.apiKey,
+      '--api-secret',
+      argv.apiSecret,
+    ],
+    params
+  );
+  command.on('error', (err) => {
+    reject(err);
+  });
+  command.on('close', (code) => {
+    if (code === 0) {
+      resolve();
+    } else {
+      reject(`exit code is ${code}`);
+    }
+  });
+}).then(() =>
+  pify(mkdirp)(path.join('dist', 'pkg'))
+).then(() =>
+  cpy([path.join('dist', '*.xpi')], path.join('dist', 'pkg'))
+).catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
