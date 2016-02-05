f6a35fd8aec8bc48a9c0b2ea34f76879c2cdc296
sanemat <o.gata.ken@gmail.com>
2016-02-05 20:11:06 +0900

    chore(deps): use browser-selected-text

--- data/content-script.js
+++ data/content-script.js
@@ -1,5 +1,6 @@
 self.port.on('buildLinkPlain', () => {
-  const selected = window.getSelection().toString();
+  // browser-selected-text https://www.npmjs.com/package/browser-selected-text
+  const selected = browserSelectedText();
   const title = window.document.title;
   const url = window.location.href;
   const result = [];
